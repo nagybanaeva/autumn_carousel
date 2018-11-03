d5e36d61c811910b3ba32c6db3a76a80088c2a32
Nagy-Bana Éva <nagybanaeva@gmail.com>
2018-11-03 22:44:23 +0100

    add js file

---
+++ script.js
@@ -0,0 +1,8 @@
+var answer = prompt("Szereted az őszt? Igen vagy nem?");
+if (answer == "igen") {
+  alert("Akkor ezt az oldalt Neked találtam ki. Jó szórakozást kívánok hozzá!");
+}
+
+else {
+  alert("Sajnálom, akkor ez az összeállítás nem Neked lett kitalálva.");
+}
